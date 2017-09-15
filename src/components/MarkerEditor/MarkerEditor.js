b709e28080e0b928c849230ca58b04edc5c9c03b
Nadzhafov <pavel.nadzhafov@primeconcept.co.uk>
2017-09-15 17:53:48 +0300

    start editor window

--- src/components/MarkerEditor/MarkerEditor.js
+++ src/components/MarkerEditor/MarkerEditor.js
@@ -1,31 +1,42 @@
-import React, { Component } from 'react'
-import { Button, Header, Icon, Modal } from 'semantic-ui-react'
+import React, {Component} from 'react'
+import { Button, Header,  Modal, Form } from 'semantic-ui-react'
+import FileInput from 'react-file-input';
 
-export default class ModalExampleControlled extends Component {
-    state = { modalOpen: false }
 
-    handleOpen = () => this.setState({ modalOpen: true })
+const options = [
+    { key: 'm', text: 'Male', value: 'male' },
+    { key: 'f', text: 'Female', value: 'female' },
+]
 
-    handleClose = () => this.setState({ modalOpen: false })
+class MarkerEditor extends Component{
+    state = {};
 
-    render() {
+    handleChange = ( e, { value } )=>this.setState( { value } );
+
+    fileInputChange = event=>{
+        console.log('Selected file:', event.target.files[0])
+    }
+
+    render(){
+        const { value } = this.state
         return (
-            <Modal
-                open={true}
-                onClose={this.handleClose}
-                basic
-                size='small'
-            >
-                <Header icon='browser' content='Cookies policy' />
+            <Modal open={true}>
+                <Modal.Header>Marker Editor</Modal.Header>
                 <Modal.Content>
-                    <h3>This website uses cookies to ensure the best user experience.</h3>
+                    <Form>
+                        <Form.Input label='Title' placeholder='Event name'/>
+                        <Form.TextArea label='Description' placeholder='Tell us more about you...'/>
+                        <FileInput name="Main Image"
+                                   accept=".png,.gif"
+                                   placeholder="Main Image"
+                                   className="inputClass"
+                                   onChange={this.fileInputChange} />
+                        <Form.Button>Submit</Form.Button>
+                    </Form>
                 </Modal.Content>
-                <Modal.Actions>
-                    <Button color='green' onClick={this.handleClose} inverted>
-                        <Icon name='checkmark' /> Got it
-                    </Button>
-                </Modal.Actions>
             </Modal>
         )
     }
 }
+
+export default MarkerEditor
