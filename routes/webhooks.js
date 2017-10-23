24ea2caf1145e8c4a4d999a9e271f23dbfa642bc
Nadzhafov <pavel.nadzhafov@primeconcept.co.uk>
2017-10-23 14:34:16 +0300

    webhooks update

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -16,7 +16,7 @@ const missingArgument = (res, name) =>{ errorResponse(res,`missing parameter ${n
 
 router.post('/socialLogin', async ( req, res )=>{
     //try {
-        successResponse(req.body);
+        successResponse(res, req.body);
         let {
           authBy = missingArgument( res, 'authBy' ),
           token = missingArgument( res, 'token' ),
