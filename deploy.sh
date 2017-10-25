6119d812fb4667fd66a5e5753f30ee99e1c7b1ec
Nadzhafov <pavel.nadzhafov@primeconcept.co.uk>
2017-10-25 15:33:01 +0300

    'auto-commit'

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,6 @@ set PATH "/var/spotw"
 set HOST "185.98.87.26"
 set PASSWORD "epidemicpassword"
 
-
 spawn ssh $USER@$HOST;
 expect "assword:";
 send "$PASSWORD\r";
@@ -15,7 +14,9 @@ send "cd $PATH\r";
 expect "#*";
 send "git pull\r";
 expect "#*";
-send "npm run build\r";
+send "forever restart app.js\r";
+expect "#*";
+send "exit\n"
 
 
 
